2de276bed0b90d368b05d8b6cf3c2a8d7364ec34
Andres Felipe Herrera Motta <131306681+AndresHerrera2023@users.noreply.github.com>
2025-08-22 16:29:18 -0500

    Update 3_occurrences_in_protected_areas.R

--- Scripts/3_occurrences_in_protected_areas.R
+++ Scripts/3_occurrences_in_protected_areas.R
@@ -1,17 +1,22 @@
 ### Code: Extract occurrences in protected areas
 ### Project: Neotropical dry forest bees
-### Author: Andres Herrera
+### Authors: Herrera-Motta et al. 
+### Last update: 08/20/25
+
 
 #Required libraries:
 library(terra)
 
+setwd("./NDF_bees_project/")
+
 #DRYFLOR:
+
 #Protected Areas based on DRYFLOR:
-pa_dryflor <- vect("./NDF_bees_project/Shapefiles/Protected_areas/dryflor/dryflor_protected_areas.shp") 
+pa_dryflor <- vect("./Shapefiles/Protected_areas/dryflor/dryflor_protected_areas.shp") 
 plot(pa_dryflor, col = "black")
 
 #DRYFLOR occurrences:
-occ_dryflor <- read.csv("./NDF_bees_project/Data/STDF_bees_occ/dryflor/species/dryflor_species_occ.csv")
+occ_dryflor <- read.csv("./Data/STDF_bees_occ/dryflor/species/dryflor_species_occ.csv")
 occ_dryflor <- vect(occ_dryflor, geom = c("longitude", "latitude"), crs="+proj=longlat +datum=WGS84")
 
 occ_pa_dryflor <- crop(occ_dryflor, pa_dryflor)
@@ -21,14 +26,15 @@ occ_dryflor_pa <- as.data.frame(occ_pa_dryflor)
 occ_dryflor_pa <- cbind(crds(occ_pa_dryflor), occ_dryflor_pa)
 colnames(occ_dryflor_pa)[c(1,2)] <- c("longitude", "latitude")
 occ_dryflor_pa <- occ_dryflor_pa[,c(3,1,2,4,5,6,7,8)]
-write.csv(occ_dryflor_pa,"Z:/Andres/NDF_bees_project/summary_diversity/dryflor/dryflor_occ_protected_areas.csv", row.names = F)
+write.csv(occ_dryflor_pa,"./summary_diversity/dryflor/dryflor_occ_protected_areas.csv", row.names = F)
 
 #TEOW:
+
 #Protected Areas based on TEOW:
-pa_teow <- vect("Z:/Andres/NDF_bees_project/Shapefiles/Protected_areas/teow/teow_protected_areas2.shp") 
+pa_teow <- vect("./Shapefiles/Protected_areas/teow/teow_protected_areas2.shp") 
 plot(pa_teow, col = "black")
 
-occ_teow <- read.csv("Z:/Andres/NDF_bees_project/Data/STDF_bees_occ/teow/species/teow_species_occ.csv")
+occ_teow <- read.csv("./Data/STDF_bees_occ/teow/species/teow_species_occ.csv")
 occ_teow <- vect(occ_teow, geom = c("longitude", "latitude"), crs="+proj=longlat +datum=WGS84")
 
 occ_pa_teow <- crop(occ_teow, pa_teow)
@@ -38,9 +44,6 @@ occ_teow_pa <- as.data.frame(occ_pa_teow)
 occ_teow_pa <- cbind(crds(occ_pa_teow), occ_teow_pa)
 colnames(occ_teow_pa)[c(1,2)] <- c("longitude", "latitude")
 occ_teow_pa <- occ_teow_pa[,c(3,1,2,4,5,6,7,8)]
-write.csv(occ_teow_pa,"Z:/Andres/NDF_bees_project/summary_diversity/teow/teow_occ_protected_areas.csv", row.names = F)
-
-
-
+write.csv(occ_teow_pa,"./summary_diversity/teow/teow_occ_protected_areas.csv", row.names = F)
 
 
