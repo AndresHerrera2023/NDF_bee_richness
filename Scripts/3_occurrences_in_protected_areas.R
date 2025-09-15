c972008ed24b55f1351b9c34736d3843358a6dd2
Andres Felipe Herrera Motta <131306681+AndresHerrera2023@users.noreply.github.com>
2025-09-15 17:38:05 -0500

    Update 3_occurrences_in_protected_areas.R

--- Scripts/3_occurrences_in_protected_areas.R
+++ Scripts/3_occurrences_in_protected_areas.R
@@ -1,7 +1,7 @@
 ### Code: Extract occurrences in protected areas
 ### Project: Neotropical dry forest bees
 ### Authors: Herrera-Motta et al. 
-### Last update: 08/20/25
+### Last update: 09/11/25
 
 
 #Required libraries:
@@ -16,9 +16,9 @@ pa_dryflor <- vect("./Shapefiles/Protected_areas/dryflor/dryflor_protected_areas
 plot(pa_dryflor, col = "black")
 
 #DRYFLOR occurrences:
-occ_dryflor <- read.csv("./Data/STDF_bees_occ/dryflor/species/dryflor_species_occ.csv")
+occ_dryflor <- read.csv("./Data/STDF_bees_occ/dryflor/all/dryflor_bees_final_occ.csv")
+occ_dryflor <- occ_dryflor[occ_dryflor$species != "sp",]
 occ_dryflor <- vect(occ_dryflor, geom = c("longitude", "latitude"), crs="+proj=longlat +datum=WGS84")
-
 occ_pa_dryflor <- crop(occ_dryflor, pa_dryflor)
 plot(occ_pa_dryflor)
 
@@ -32,9 +32,8 @@ write.csv(occ_dryflor_pa,"./summary_diversity/dryflor/dryflor_occ_protected_area
 
 #Protected Areas based on TEOW:
 pa_teow <- vect("./Shapefiles/Protected_areas/teow/teow_protected_areas2.shp") 
-plot(pa_teow, col = "black")
 
-occ_teow <- read.csv("./Data/STDF_bees_occ/teow/species/teow_species_occ.csv")
+occ_teow <- read.csv("./Data/STDF_bees_occ/teow/all/teow_bees_final_occ.csv")
 occ_teow <- vect(occ_teow, geom = c("longitude", "latitude"), crs="+proj=longlat +datum=WGS84")
 
 occ_pa_teow <- crop(occ_teow, pa_teow)
@@ -45,5 +44,4 @@ occ_teow_pa <- cbind(crds(occ_pa_teow), occ_teow_pa)
 colnames(occ_teow_pa)[c(1,2)] <- c("longitude", "latitude")
 occ_teow_pa <- occ_teow_pa[,c(3,1,2,4,5,6,7,8)]
 write.csv(occ_teow_pa,"./summary_diversity/teow/teow_occ_protected_areas.csv", row.names = F)
-
 
